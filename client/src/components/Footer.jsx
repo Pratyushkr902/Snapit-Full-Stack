af650b8622348295c4a9e776a32a34815ded8af7
Pratyush Sharma <00pratyush20@gmail.com>
2026-06-02 03:42:05 +0530

    feat: add LinkedIn icon to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className='border-t'>
         <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
             <p>© All Rights Reserved 2026 Snapit.</p>
-
             <div className='flex items-center gap-4 justify-center text-2xl'>
                 <a 
                     href='https://www.facebook.com/share/18RV5RTPMc/?mibextid=wwXIfr' 
@@ -24,6 +24,14 @@ const Footer = () => {
                     className='hover:text-pink-500 transition-colors'
                 >
                     <FaInstagram/>
+                </a>
+                <a 
+                    href='https://www.linkedin.com/company/snapitnow' 
+                    target='_blank' 
+                    rel='noreferrer'
+                    className='hover:text-blue-700 transition-colors'
+                >
+                    <FaLinkedin/>
                 </a>
             </div>
         </div>
