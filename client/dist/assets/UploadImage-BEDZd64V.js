e22bf362d1f50499e9712ed90697124a86171344
Pratyush Sharma <00pratyush20@gmail.com>
2026-06-14 14:59:04 +0530

    chore: rebuild dist

--- client/dist/assets/UploadImage-BEDZd64V.js
+++
@@ -1,1 +0,0 @@
-import{A as o,S as e}from"./index-BqJgH0R1.js";const n=async r=>{try{const a=new FormData;return a.append("image",r),await o({...e.uploadImage,data:a})}catch(a){return a}};export{n as u};
