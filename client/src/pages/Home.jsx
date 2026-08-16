a4b56fd5144faef7b8cd71d6d010b0c207b2b44d
Pratyush Sharma <00pratyush20@gmail.com>
2026-08-17 00:21:23 +0530

    Add store-closed banner (9pm-8am) with Tabler icons

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -84,7 +84,9 @@ const Home = () => {
   return (
     <section className='bg-white min-h-screen overflow-x-hidden'>
 
-            <StoreClosedOverlay />   {/* ← add this line */}
+      {/* 0. STORE CLOSED BANNER (9pm-8am IST, hidden for admin/seller/rider) */}
+      <StoreClosedOverlay />
+
       {/* 1. BANNER */}
       <div className='container mx-auto px-0 lg:px-4 mb-3 lg:mb-5'>
         <HomeBanner />
