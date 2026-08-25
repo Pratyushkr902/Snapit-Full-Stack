765a6ccefd1801425b2ce327a27d2cd923bdcff5
Pratyush Sharma <00pratyush20@gmail.com>
2026-08-26 02:36:48 +0530

    fix: correct superAdmin middleware import path in festiveOffer.route.js

--- server/route/festiveOffer.route.js
+++ server/route/festiveOffer.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { superAdmin } from "../middleware/superAdmin.js";
+import { superAdmin } from "../middleware/Admin.js";
 import {
   getFestiveOfferController,
   updateFestiveOfferController,
