34241a65b9bb83b6e9aa3a66cecba31495c5d2d5
Pratyush Sharma <00pratyush20@gmail.com>
2026-08-29 00:13:27 +0530

    chore: sync appVersion constant to 68 and rebuild release bundles

--- client/src/constants/appVersion.js
+++ client/src/constants/appVersion.js
@@ -1,3 +1,3 @@
 // Client App Version & Build Configuration
-export const CURRENT_APP_VERSION = '2.6.28';
-export const CURRENT_VERSION_CODE = 67;
+export const CURRENT_APP_VERSION = '2.6.29';
+export const CURRENT_VERSION_CODE = 68;
