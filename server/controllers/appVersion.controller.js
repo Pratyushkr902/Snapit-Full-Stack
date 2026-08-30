135fe5c2f62f0b7d79e02101d5e55efe4b123280
Pratyush Sharma <00pratyush20@gmail.com>
2026-08-30 12:00:34 +0530

    feat: enforce mandatory update to version 72 (v2.6.33)

--- server/controllers/appVersion.controller.js
+++ server/controllers/appVersion.controller.js
@@ -7,12 +7,12 @@ export const getAppVersionController = async (req, res) => {
       data: {
         latestVersion: "2.6.33",
         latestVersionCode: 72,
-        minRequiredVersionCode: 70,
-        forceUpdate: false,
+        minRequiredVersionCode: 72,
+        forceUpdate: true,
         playStoreUrl: "https://play.google.com/store/apps/details?id=com.snapit.grocery",
         directApkUrl: "https://snapit.pages.dev/app-release.apk",
         title: "Update Available 🚀",
-        message: "A new version of Snapit (v2.6.31) is ready with faster 10-minute delivery, smart search & new features!",
+        message: "A new version of Snapit (v2.6.33) is ready with faster 10-minute delivery, smart search & new features!",
         releaseNotes: [
           "⚡ Faster 10-Minute Delivery & Order Tracking",
           "🔍 Blinkit/Zepto-Grade Smart Voice Search & Hindi Filters",
