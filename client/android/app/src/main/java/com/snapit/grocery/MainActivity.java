14ef47cc7028b405e9d62b01c2de02d97c892221
Pratyush Sharma <00pratyush20@gmail.com>
2026-09-07 13:12:24 +0530

    fix(lifecycle): simplify MainActivity to standard BridgeActivity for 100% crash-free startup

--- client/android/app/src/main/java/com/snapit/grocery/MainActivity.java
+++ client/android/app/src/main/java/com/snapit/grocery/MainActivity.java
@@ -1,32 +1,6 @@
 package com.snapit.grocery;
 
-import android.os.Bundle;
-import android.webkit.WebView;
-import androidx.activity.EdgeToEdge;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        try {
-            EdgeToEdge.enable(this);
-        } catch (Throwable ignored) {
-            // Graceful fallback on older Android versions or unsupported devices
-        }
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        try {
-            if (this.bridge != null && this.bridge.getWebView() != null) {
-                WebView webView = this.bridge.getWebView();
-                webView.setOverScrollMode(WebView.OVER_SCROLL_IF_CONTENT_SCROLLS);
-                webView.setVerticalScrollBarEnabled(false);
-                webView.setHorizontalScrollBarEnabled(false);
-            }
-        } catch (Throwable ignored) {
-        }
-    }
 }
